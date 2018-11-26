5b9675bc350ed1bffeb3951ef2248ee044dbaf6f
Aaska Shah <aaska18@gmail.com>
2018-11-25 23:55:44 -0500

    extended payment with coins behaviour to refund ticket process

--- Source/resources/scripts.js
+++ Source/resources/scripts.js
@@ -1,70 +1,130 @@
 var changeVal;
 function nickel(){
-	var value = document.getElementById("payVal").innerHTML.slice(1);
-	//window.alert(value);
-	value = value - 0.05;
-	document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
-	if(value <= 0){
-		changeVal = value.toFixed(2).slice(1);
-		window.location = 'paymentSuccessful.html';
-		localStorage.setItem(1, changeVal);
+	if(document.getElementById("payVal")){
+		var value = document.getElementById("payVal").innerHTML.slice(1);
+		value = value - 0.05;
+		document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			pay();
+		}
+	}
+	else if (document.getElementById("fundVal")){
+		var value = document.getElementById("fundVal").innerHTML.slice(1);
+		value = value - 0.05;
+		document.getElementById("fundVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			fund(value);
+		
+		}
 	}
 }
 
 function dime(){
-	var value = document.getElementById("payVal").innerHTML.slice(1);
-	//window.alert(value);
-	value = value - 0.10;
-	document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
-	if(value <= 0){
-		changeVal = value.toFixed(2).slice(1);
-		window.location = 'paymentSuccessful.html';
-		localStorage.setItem(1, changeVal);
+	if(document.getElementById("payVal")){
+		var value = document.getElementById("payVal").innerHTML.slice(1);
+		//window.alert(value);
+		value = value - 0.10;
+		document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			pay();
+		}
+	}
+	else if (document.getElementById("fundVal")){
+		var value = document.getElementById("fundVal").innerHTML.slice(1);
+		value = value - 0.10;
+		document.getElementById("fundVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			fund(value);
+		
+		}
 	}
 }
 
 function quarter(){
-	var value = document.getElementById("payVal").innerHTML.slice(1);
-	//window.alert(value);
-	value = value - 0.25;
-	document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
-	if(value <= 0){
-		changeVal = value.toFixed(2).slice(1);
-		window.location = 'paymentSuccessful.html';
-		localStorage.setItem(1, changeVal);
+	if(document.getElementById("payVal")){
+		var value = document.getElementById("payVal").innerHTML.slice(1);
+		value = value - 0.25;
+		document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			pay();
+		}
+	}
+
+	else if (document.getElementById("fundVal")){
+		var value = document.getElementById("fundVal").innerHTML.slice(1);
+		value = value - 0.25;
+		document.getElementById("fundVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			fund(value);
+		
+		}
 	}
 }
 
 function loonie(){
-	var value = document.getElementById("payVal").innerHTML.slice(1);
-	//window.alert(value);
-	value = value - 1;
-	document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
-	if(value <= 0){
-		changeVal = value.toFixed(2).slice(1);
-		window.location = 'paymentSuccessful.html';
-		localStorage.setItem(1, changeVal);
+	if(document.getElementById("payVal")){
+		var value = document.getElementById("payVal").innerHTML.slice(1);
+		value = value - 1;
+		document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			pay();
+		}
+	}
+	else if (document.getElementById("fundVal")){
+		var value = document.getElementById("fundVal").innerHTML.slice(1);
+		value = value - 1;
+		document.getElementById("fundVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			fund(value);
+		
+		}
 	}
 }
 
 
 function toonie(){
-	var value = document.getElementById("payVal").innerHTML.slice(1);
-	//window.alert(value);
-	value = value - 2;
-	document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
-	if(value <= 0){
-		changeVal = value.toFixed(2).slice(1);
-		window.location = 'paymentSuccessful.html';
-		localStorage.setItem(1, changeVal);
-	
+	if(document.getElementById("payVal")){
+		var value = document.getElementById("payVal").innerHTML.slice(1);
+		value = value - 2;
+		document.getElementById("payVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			pay(value);
+		
+		}
 	}
+	else if (document.getElementById("fundVal")){
+		var value = document.getElementById("fundVal").innerHTML.slice(1);
+		value = value - 2;
+		document.getElementById("fundVal").innerText = '$' + value.toFixed(2);	
+		if(value <= 0){
+			fund(value);
+		
+		}
+	}
+	
+}
+
+function pay(value){
+	changeVal = value.toFixed(2).slice(1);
+	window.location = 'paymentSuccessful.html';
+	localStorage.setItem("pay", changeVal);
+}
+
+function fund(value){
+	changeVal = value.toFixed(2).slice(1);
+	window.location = 'fundsAdded.html';
+	localStorage.setItem("fund", changeVal);	
 }
 
 function change(){
-		
+	if(document.getElementById("paySucc")){
 		var value = document.getElementById("paySucc").innerHTML;
-		document.getElementById("paySucc").innerText = 'Change Due: $' + localStorage.getItem(1) ;
+		document.getElementById("paySucc").innerText = 'Change Due: $' + localStorage.getItem("pay") ;
+	}
+	else if(document.getElementById("fundsAdded")){
+		var fund = document.getElementById("fundsAdded").innerHTML;
+		document.getElementById("fundsAdded").innerText = 'Change Due: $' + localStorage.getItem("fund") ;
+	}
 
 }
 
